1a1e34f727639e0a3df3cb15db61e6fbbbd74489
Kasper van den Berg <kasper@kaspervandenberg.net>
2021-03-01 23:15:03 +0100

    0000 Package alias and exports

--- src/package.lisp
+++ src/package.lisp
@@ -1,8 +1,12 @@
 ;; © Kasper van den Berg, 2021
 (defpackage net.kaspervandenberg.behaviour-tree
+  (:nicknames :behaviour-tree)
   (:use :cl)
   (:export behaviour
 	   task
+	   behaviour-sequence
+	   selector
 	   initialise
 	   tick
-	   terminate))
+	   terminate
+	   defaction))
